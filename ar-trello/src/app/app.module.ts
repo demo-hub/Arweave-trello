6d828eb87b3bfd8f11349cbd745c2ac42035b383
João Rodrigues <joao.rodrigues@inova.business>
2020-12-11 23:14:56 +0000

    trello clone setup

--- ar-trello/src/app/app.module.ts
+++ ar-trello/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { ListComponent } from './list/list.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    ListComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
