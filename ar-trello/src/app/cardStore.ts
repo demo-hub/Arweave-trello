28321ff8121100816390a1eb20eee800c5ffa889
João Rodrigues <joao.rodrigues@inova.business>
2020-12-21 15:41:38 +0000

    change init of arweave to work on the server

--- ar-trello/src/app/cardStore.ts
+++ ar-trello/src/app/cardStore.ts
@@ -6,7 +6,13 @@ import { and, or, equals } from 'arql-ops';
 
 // Since v1.5.1 you're now able to call the init function for the web version without options.
 // The current path will be used by default, recommended.
-const arweave = Arweave.init();
+const arweave = Arweave.init({
+  host: 'arweave.net', // Hostname or IP address for a Arweave host
+  port: 443,          // Port
+  protocol: 'https',  // Network protocol http or https
+  timeout: 20000,     // Network request timeouts in milliseconds
+  logging: false,     // Enable network request logging
+});
 export class CardStore {
   cards: object = {};
   lastid = -1;
